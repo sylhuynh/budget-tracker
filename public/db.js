91f3b5231026186b1e4611ed9c878a2a3889ed52
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-05-04 19:15:02 -0700

    added onsuccess function to store transactions in db and checkDatabase function

--- public/db.js
+++ public/db.js
@@ -22,13 +22,41 @@ request.onerror = function (event) {
   console.log(event);
 };
 
-// TODO: add code so that any transactions stored in the db
-// are sent to the backend if/when the user goes online
-// Hint: learn about "navigator.onLine" and the "online" window event.
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
 
 // TODO: add code to saveRecord so that it accepts a record object for a
 // transaction and saves it in the db. This function is called in index.js
 // when the user creates a transaction while offline.
 function saveRecord(record) {
   // add your code here
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.objectStore("pending");
+  const getAll = pendingStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const pendingStore = transaction.objectStore("pending");
+          pendingStore.clear();
+        });
+    }
+  };
 }
